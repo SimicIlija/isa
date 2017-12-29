e2f0f6226eaf17d0362163be18607cb7590356e3
SimicIlija <simicsilija@gmail.com>
2017-12-29 11:50:53 +0100

    Spring boot napravljen

---
+++ src/main/java/com/isa/projekcije/ProjekcijeApplication.java
@@ -0,0 +1,12 @@
+package com.isa.projekcije;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekcijeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekcijeApplication.class, args);
+	}
+}
