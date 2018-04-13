abd3bf5fce2c9230133340ba7bab8d9c2244e2fe
SimicIlija <simicsilija@gmail.com>
2018-04-14 00:51:00 +0200

    Zapoceti korisnicki rekviziti

---
+++ src/main/java/com/isa/projekcije/model/dto/UserPropsDto.java
@@ -0,0 +1,53 @@
+package com.isa.projekcije.model.dto;
+
+import com.isa.projekcije.model.fanzone.UserProps;
+import com.isa.projekcije.model.fanzone.UserPropsState;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+public class UserPropsDto {
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
+    @NotNull
+    private Date endDate;
+
+    public UserPropsDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public UserProps createUserProps() {
+        UserProps userProps = new UserProps();
+        userProps.setState(UserPropsState.REJECTED);
+        userProps.setName(this.name);
+        userProps.setDescription(this.description);
+        userProps.setEndDate(this.endDate);
+        return userProps;
+    }
+}
--- src/main/java/com/isa/projekcije/model/fanzone/UserProps.java
+++ src/main/java/com/isa/projekcije/model/fanzone/UserProps.java
@@ -1,4 +1,88 @@
 package com.isa.projekcije.model.fanzone;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class UserProps {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserPropsState state;
+
+    private String imageUrl;
+
+    public UserProps() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public UserPropsState getState() {
+        return state;
+    }
+
+    public void setState(UserPropsState state) {
+        this.state = state;
+    }
 }
