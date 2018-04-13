ffbefea1765c0e0d8198c896d65fe46d45880b3b
Jelena <jelena_stanarevic@uns.ac.rs>
2018-04-13 14:26:09 +0200

    Uradjene stranice za login i registraciju.

---
+++ src/main/webapp/view/js/login.js
@@ -0,0 +1,36 @@
+function getFormData($form) {
+    var unordered_array = $form.serializeArray();
+    var ordered_array = {};
+
+    $.map(unordered_array, function (n, i) {
+        ordered_array[n['name']] = n['value'];
+    });
+    return ordered_array;
+}
+
+
+function logInUser() {
+    $form = $("#loginForm");
+    var data = getFormData($form);
+    var s = JSON.stringify(data);
+    $.ajax({
+        url: "user/login",
+        type: "POST",
+        data: s,
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+            if (data == false) {
+                toastr["error"]("Username/password is incorrect,doesn't exist or empty");
+
+            } else {
+                //top.location.href="index.html";
+                alert('uspeo!');
+            }
+        }, error: function (jqxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+
+    });
+
+}
