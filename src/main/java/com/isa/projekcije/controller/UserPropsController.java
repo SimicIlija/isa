f06b106bace5f56ddd5256002d9cdd0850d89110
SimicIlija <simicsilija@gmail.com>
2018-04-15 22:06:18 +0200

    Upload slike za korisnicke rekvizite

--- src/main/java/com/isa/projekcije/controller/UserPropsController.java
+++ src/main/java/com/isa/projekcije/controller/UserPropsController.java
@@ -4,13 +4,17 @@ import com.isa.projekcije.model.User;
 import com.isa.projekcije.model.dto.UserPropsDto;
 import com.isa.projekcije.model.fanzone.UserProps;
 import com.isa.projekcije.model.fanzone.UserPropsState;
+import com.isa.projekcije.service.StorageService;
 import com.isa.projekcije.service.UserPropsService;
 import com.isa.projekcije.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -23,6 +27,9 @@ public class UserPropsController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private StorageService storageService;
 
     /**
      * GET api/userprops
@@ -86,6 +93,42 @@ public class UserPropsController {
         } catch (Exception e) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
+    }
 
+    /**
+     * POST api/userprops/{id}/image
+     * Upload image for user props with path variable id
+     * TODO : Check if request is sent by user props creator
+     */
+    @RequestMapping(value = "/{id}/image", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity uploadFile(@PathVariable long id, @RequestParam("file") MultipartFile file) {
+        try {
+            UserProps userProps = userPropsService.findById(id);
+            String imageUrl = storageService.store(file);
+            userProps.setImageUrl(imageUrl);
+            userPropsService.update(userProps);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(e, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * GET api/userprops/{id}/image
+     * Download image for user props with path variable id
+     */
+    @RequestMapping(value = "/{id}/image", method = RequestMethod.GET)
+    public ResponseEntity<Resource> getFile(@PathVariable long id) {
+        try {
+            UserProps userProps = userPropsService.findById(id);
+            String imageUrl = userProps.getImageUrl();
+            Resource resource = storageService.loadFile(imageUrl);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
