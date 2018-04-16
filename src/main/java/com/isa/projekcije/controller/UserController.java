ad94420857a85dc23fce61d03c186ad5b9f31986
SimicIlija <simicsilija@gmail.com>
2018-04-16 23:10:05 +0200

    Ispravljen login

--- src/main/java/com/isa/projekcije/controller/UserController.java
+++ src/main/java/com/isa/projekcije/controller/UserController.java
@@ -1,27 +1,21 @@
 package com.isa.projekcije.controller;
 
-import com.isa.projekcije.model.ServletContextImpl;
 import com.isa.projekcije.model.User;
 import com.isa.projekcije.model.dto.LoginDTO;
 import com.isa.projekcije.model.dto.RegistrationDTO;
+import com.isa.projekcije.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import com.isa.projekcije.service.UserService;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-
 
 
     @Autowired
@@ -36,10 +30,10 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
         User user = this.userService.findUser(loginDTO);
 
-        if (user != null && user.getPassword().equals(loginDTO.getPassword())) {
+        if (user != null) {
             userService.setCurrentUser(user);
 
-            return new ResponseEntity(user, HttpStatus.OK);
+            return new ResponseEntity<>(user, HttpStatus.OK);
         }
 
         return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -53,22 +47,28 @@ public class UserController {
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> registerUser(@RequestBody RegistrationDTO registrationDTO) {
         System.out.println(registrationDTO.getFirstName());
-        if(registrationDTO.isEmpty()){
-            return new ResponseEntity(new RuntimeException("Missing mandatory fields!"),HttpStatus.BAD_REQUEST);
+        if (registrationDTO.isEmpty()) {
+            return new ResponseEntity(new RuntimeException("Missing mandatory fields!"), HttpStatus.BAD_REQUEST);
         }
 
-        if(!(registrationDTO.correctPassword())){
-            return new ResponseEntity(new RuntimeException("Passwords do not match!"),HttpStatus.BAD_REQUEST);
+        if (!(registrationDTO.correctPassword())) {
+            return new ResponseEntity(new RuntimeException("Passwords do not match!"), HttpStatus.BAD_REQUEST);
         }
 
-        if(userService.emailExists(registrationDTO)){
-            return new ResponseEntity(new RuntimeException("Email already exists!"),HttpStatus.BAD_REQUEST);
+        if (userService.emailExists(registrationDTO)) {
+            return new ResponseEntity(new RuntimeException("Email already exists!"), HttpStatus.BAD_REQUEST);
         }
         User registeredUser = userService.createUser(registrationDTO);
         System.out.println(registeredUser.getFirstName());
-        return new ResponseEntity<>(registeredUser,HttpStatus.CREATED);
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
 
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/signout")
+    public ResponseEntity signout() {
+        SecurityContextHolder.clearContext();
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/isa/projekcije/service/UserService.java
+++ src/main/java/com/isa/projekcije/service/UserService.java
@@ -9,9 +9,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -36,20 +43,20 @@ public class UserService {
         return this.userRepository.findByEmail(email);
     }
 
-    public User createUser(RegistrationDTO registrationDTO){
+    public User createUser(RegistrationDTO registrationDTO) {
         User registeredUser = registrationDTO.createNewUser();
         System.out.println(registeredUser.getFirstName());
         return userRepository.save(registeredUser);
     }
 
-    public boolean emailExists(RegistrationDTO registrationDTO){
+    public boolean emailExists(RegistrationDTO registrationDTO) {
         if (userRepository.findByEmail(registrationDTO.getEmail()) != null) {
             return true;
         }
         return false;
     }
 
-    public User findUser(LoginDTO loginDTO){
+    public User findUser(LoginDTO loginDTO) {
         System.out.println(userRepository.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword()));
         return userRepository.findByEmailAndPassword(loginDTO.getEmail(), loginDTO.getPassword());
     }
@@ -57,13 +64,6 @@ public class UserService {
     public List<User> getAllUsers() {
 
         List<User> users = userRepository.findAll(new Sort("email"));
-        /*User user = (User) request.getSession().getAttribute("user");
-            for(User u : users){
-                if(u.getEmail().equals(user.getEmail())){
-                    users.remove(u);
-                    break;
-                }
-            }*/
         return users;
 
     }
@@ -86,27 +86,20 @@ public class UserService {
     }
 
     public void setCurrentUser(User user) {
-        request.getSession().setAttribute("user", user);
-        /* Collection<GrantedAuthority> authorities = new ArrayList<>();
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
-         Authentication authentication = new PreAuthenticatedAuthenticationToken(user.getId(), null, authorities);
-        SecurityContextHolder.getContext().setAuthentication(authentication);*/
-
+        Authentication authentication = new PreAuthenticatedAuthenticationToken(user.getId(), null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     public User getCurrentUser() {
-        return (User) request.getSession().getAttribute("user");
-         /*Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(auth.getName());
-        if(auth.getName().equals("anonymousUser")){
-            return null;
-        }
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         try {
             Long id = Long.parseLong(auth.getName());
-            return userRepository.findById(id);
+            return userRepository.findById(id).orElseThrow(RuntimeException::new);
         } catch (Exception e) {
            return null;
-        }*/
+        }
     }
 
 
