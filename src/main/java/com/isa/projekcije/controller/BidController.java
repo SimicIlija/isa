e42945db52181c7112d8e74cac0390b35a9c6ce3
SimicIlija <simicsilija@gmail.com>
2018-04-18 00:22:52 +0200

    Ponude bez provere korisnika

--- src/main/java/com/isa/projekcije/controller/BidController.java
+++ src/main/java/com/isa/projekcije/controller/BidController.java
@@ -4,6 +4,7 @@ import com.isa.projekcije.model.User;
 import com.isa.projekcije.model.fanzone.Bid;
 import com.isa.projekcije.model.fanzone.BidState;
 import com.isa.projekcije.model.fanzone.UserProps;
+import com.isa.projekcije.model.fanzone.UserPropsState;
 import com.isa.projekcije.service.BidService;
 import com.isa.projekcije.service.UserPropsService;
 import com.isa.projekcije.service.UserService;
@@ -36,15 +37,70 @@ public class BidController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity getAll() {
         List<Bid> result = bidService.findAll();
-        return new ResponseEntity(result, HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    /**
+     * GET api/bid/up/{id}
+     * Returns all bids where user props id is equal to id
+     */
+    @RequestMapping(value = "/up/{id}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getByUserPropsId(@PathVariable("id") long id) {
+        try {
+            List<Bid> bids = bidService.findByUserProps(id);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * GET api/bid/bidder/{id}
+     * Returns all bids where bidder id is equal to id
+     */
+    @RequestMapping(value = "/bidder/{id}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getByBidderId(@PathVariable("id") long id) {
+        try {
+            List<Bid> bids = bidService.findByBidderId(id);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * GET api/bid/creator/{id}
+     * Returns all bids where creator of user props id is equal to id
+     */
+    @RequestMapping(value = "/creator/{id}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getByCreatorId(@PathVariable("id") long id) {
+        try {
+            List<Bid> bids = bidService.findByCreatorId(id);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * POST api/bid/up/{id}
+     * Creating a bid for user props.
+     */
     @RequestMapping(value = "/up/{id}", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity addNewBid(@PathVariable("id") long id, @Valid @RequestBody double price) {
         try {
             UserProps userProps = userPropsService.findById(id);
+            if (userProps.getState() == UserPropsState.DENIED) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+            if (bidService.isAccepted(userProps.getId())) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
             User user = userService.findById(2);
             Bid bid = new Bid();
             bid.setBidder(user);
@@ -52,10 +108,63 @@ public class BidController {
             bid.setPrice(price);
             bid.setBidState(BidState.DEFAULT);
             bid = bidService.save(bid);
-            return new ResponseEntity(bid, HttpStatus.OK);
+            return new ResponseEntity<>(bid, HttpStatus.OK);
         } catch (Exception e) {
-            e.printStackTrace();
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
     }
+
+    /**
+     * PUT api/bid/up/{id}
+     * Updating a bid for user props.
+     */
+    @RequestMapping(value = "/up/{id}", method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity modifyBid(@PathVariable("id") long id, @Valid @RequestBody double price) {
+        try {
+            UserProps userProps = userPropsService.findById(id);
+            User user = userService.findById(2);
+            Bid bid = bidService.findById(userProps.getId(), user.getId());
+            if (bidService.isAccepted(userProps.getId())) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+            bid.setPrice(price);
+            bid.setBidState(BidState.DEFAULT);
+            bid = bidService.save(bid);
+            return new ResponseEntity<>(bid, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * PUT api/bid/up/{id1}/bidder/{id2}
+     * Updating a bid state for user props.
+     * If accepted is true, then this bid should be accepted, and other bids of same
+     */
+    @RequestMapping(value = "/up/{id1}/bidder/{id2}", method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity changeBidState(@PathVariable("id1") long userPropid,
+                                         @PathVariable("id2") long bidderId,
+                                         @Valid @RequestBody boolean accepted) {
+        try {
+            Bid bid = bidService.findById(userPropid, bidderId);
+            if (bidService.isAccepted(userPropid)) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+            if (accepted) {
+                bidService.acceptBid(bid);
+            } else {
+                bid.setBidState(BidState.REJECTED);
+                bid = bidService.save(bid);
+            }
+            return new ResponseEntity<>(bid, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 }
--- src/main/java/com/isa/projekcije/service/BidService.java
+++ src/main/java/com/isa/projekcije/service/BidService.java
@@ -1,6 +1,8 @@
 package com.isa.projekcije.service;
 
 import com.isa.projekcije.model.fanzone.Bid;
+import com.isa.projekcije.model.fanzone.BidId;
+import com.isa.projekcije.model.fanzone.BidState;
 import com.isa.projekcije.repository.BidRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +20,47 @@ public class BidService {
 
     public Bid save(Bid bid) {
         return bidRepository.save(bid);
+    }
+
+    public Bid findById(long userPropsId, long userId) {
+        BidId bidId = new BidId();
+        bidId.setUserPropsId(userPropsId);
+        bidId.setBidderId(userId);
+        return bidRepository.findByBidId(bidId).orElseThrow(RuntimeException::new);
+    }
+
+    public List<Bid> findByUserProps(long id) {
+        return bidRepository.findByUserProps_Id(id);
+    }
+
+    public List<Bid> findByBidderId(long id) {
+        return bidRepository.findByBidder_Id(id);
+    }
+
+    public List<Bid> findByCreatorId(long id) {
+        return bidRepository.findByUserProps_Creator_Id(id);
+    }
+
+    public boolean isAccepted(long userPropsId) {
+        List<Bid> list = findByUserProps(userPropsId);
+        boolean retval = false;
+        for (Bid bid : list) {
+            if (bid.getBidState() == BidState.ACCEPTED) {
+                retval = true;
+            }
+        }
+        return retval;
+    }
+
+    public void acceptBid(Bid bid) {
+        List<Bid> list = findByUserProps(bid.getUserProps().getId());
+        for (Bid itBid : list) {
+            if (itBid.getBidder() == bid.getBidder()) {
+                itBid.setBidState(BidState.ACCEPTED);
+            } else {
+                itBid.setBidState(BidState.REJECTED);
+            }
+            bidRepository.save(itBid);
+        }
     }
 }
